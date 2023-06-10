4032f791167f24603b3fd755018929b09e2bcf8e
Bertie Wheen <bertie@wheen.online>
2023-06-10 14:07:41 +0100

    update latest

--- src/routes/Latest.svelte
+++ src/routes/Latest.svelte
@@ -1,9 +1,19 @@
+<script>
+  import ExternalLink from "$lib/links/ExternalLink.svelte"
+  import YouTubeVideoLink from "$lib/links/socials/YouTubeVideoLink.svelte"
+</script>
+
 <div class="latest">
-  On Tuesday June 6th we'll once again be meeting MPs to make real progress
-  towards a new law to ensure that every one has the right to a care supporter
-  (or care partner) wherever they are in the health and care system. Dan Carden
-  (Lab) will present this as a Ten Minute Rule Bill, supported by Tracy Crouch
-  (Con), Daisy Cooper (Lib Dem) and Liz Saville-Roberts (Plaid Cymru).
+  We're delighted to report that the Right to a Care Supporter Bill presented by
+  Dan Carden on June 6 was accepted. The second reading is scheduled for
+  November 24.
+  <ExternalLink
+    href="https://hansard.parliament.uk/commons/2023-06-06/debates/26AF1B7E-0BCC-4490-9721-435E3029AFB6/CareSupporters">
+    Read the Hansard report here</ExternalLink>
+  or
+  <YouTubeVideoLink id="N0Zl2y4M5aw">
+    watch Dan's speech on YouTube</YouTubeVideoLink
+  >.
 </div>
 
 <style lang="sass">
